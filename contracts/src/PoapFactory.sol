e3ff70082f96363e466ddf1d75050fdda44a69d6
rahul.chander <rahul.chander@epitech.eu>
2025-07-12 15:09:02 +0200

    feat: Init PoapFactory to create new poaps

---
+++ contracts/src/PoapFactory.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract PoapFactory is ERC721URIStorage {
+    uint256 public nextPoapId;
+
+    struct PoapMetadata {
+        string name;
+        string description;
+        string imageIPFS;
+        uint256 durability;
+    }
+
+    mapping(uint256 => PoapMetadata) public poaps;
+
+    event PoapCreated(uint256 indexed poapId, address indexed owner, string name, string description, string imageIPFS);
+
+    constructor() ERC721("POAP", "POAP") {}
+
+    function createPoap(
+        string memory name,
+        string memory description,
+        string memory imageIPFS,
+        string memory tokenURI,
+        uint256 memory durability
+    ) public {
+        uint256 poapId = nextPoapId;
+        poaps[poapId] = PoapMetadata(name, description, imageIPFS);
+
+        _mint(msg.sender, poapId);
+        _setTokenURI(poapId, tokenURI);
+
+        emit PoapCreated(poapId, msg.sender, name, description, imageIPFS);
+
+        nextPoapId++;
+    }
+
+    function getPoap(uint256 poapId) public view returns (PoapMetadata memory) {
+        return poaps[poapId];
+    }
+}
